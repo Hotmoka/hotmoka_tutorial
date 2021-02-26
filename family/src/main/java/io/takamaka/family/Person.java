d62f9511755647e853afbe3a460283b0933b1cf3
Fausto Spoto <fausto.spoto@univr.it>
2021-02-26 17:07:12 +0100

    Added family project

---
+++ family/src/main/java/io/takamaka/family/Person.java
@@ -0,0 +1,30 @@
+package io.takamaka.family;
+
+public class Person {
+	private final String name;
+	private final int day;
+	private final int month;
+	private final int year;
+	public final Person parent1;
+	public final Person parent2;
+
+	public Person(String name, int day, int month, int year,
+			      Person parent1, Person parent2) {
+
+		this.name = name;
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.parent1 = parent1;
+		this.parent2 = parent2;
+	}
+
+	public Person(String name, int day, int month, int year) {
+		this(name, day, month, year, null, null);
+	}
+
+	@Override
+	public String toString() {
+		return name + " (" + day + "/" + month + "/" + year + ")";
+	}
+}
