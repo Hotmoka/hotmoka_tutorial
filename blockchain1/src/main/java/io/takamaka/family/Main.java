d21793a12e31209778ad4784fb382ac8c42c27a5
Fausto Spoto <fausto.spoto@univr.it>
2021-02-26 17:39:29 +0100

    Started the blockchain1 project

---
+++ blockchain1/src/main/java/io/takamaka/family/Main.java
@@ -0,0 +1,30 @@
+package io.takamaka.family;
+
+import java.math.BigInteger;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import io.hotmoka.memory.MemoryBlockchain;
+import io.hotmoka.memory.MemoryBlockchainConfig;
+import io.hotmoka.nodes.ConsensusParams;
+import io.hotmoka.nodes.Node;
+import io.hotmoka.nodes.views.InitializedNode;
+
+public class Main {
+  public final static BigInteger GREEN_AMOUNT = BigInteger.valueOf(100_000_000);
+  public final static BigInteger RED_AMOUNT = BigInteger.ZERO;
+
+  public static void main(String[] args) throws Exception {
+    MemoryBlockchainConfig config = new MemoryBlockchainConfig.Builder().build();
+    ConsensusParams consensus = new ConsensusParams.Builder().build();
+
+    // the path of the packaged runtime Takamaka classes
+    Path takamakaCodePath = Paths.get
+      ("../../hotmoka/modules/explicit/io-takamaka-code-1.0.0.jar");
+
+    try (Node node = MemoryBlockchain.init(config, consensus)) {
+      InitializedNode initialized = InitializedNode.of
+        (node, consensus, takamakaCodePath, GREEN_AMOUNT, RED_AMOUNT);
+    }
+  }
+}
