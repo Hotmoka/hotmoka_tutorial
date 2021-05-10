50e772d8f352abd3a31dc5b551a7f6ddc4240fc7
Fausto Spoto <fausto.spoto@gmail.com>
2021-05-10 13:02:44 +0200

    Updated URL of server

--- runs/src/main/java/runs/Family2.java
+++ runs/src/main/java/runs/Family2.java
@@ -70,7 +70,7 @@ public class Family2 {
     Path familyPath = Paths.get("../family_storage/target/family_storage-0.0.1-SNAPSHOT.jar");
 
     RemoteNodeConfig config = new RemoteNodeConfig.Builder()
-    	.setURL("ec2-54-194-239-91.eu-west-1.compute.amazonaws.com:8080")
+    	.setURL("panarea.hotmoka.io")
     	.build();
 
     try (Node node = RemoteNode.of(config)) {
